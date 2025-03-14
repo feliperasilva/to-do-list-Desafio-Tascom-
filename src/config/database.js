ca0933eba58ce5e64280a0da6fadea49f3b2b1a4
Felipe Romero Alves da Silva <fras2@discente.ifpe.edu.br>
2025-03-14 20:49:30 -0300

    feat: preenchendo informações do database.js

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: "to-do-list",
+  define: {
+    timestamp: true,
+    underscored: true,
+  },
+};
